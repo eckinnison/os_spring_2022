802b9ff3731afffad001e4975b5cfff9c3b2ece5
Emma Kinnison <ekinnison@morbius.mscsnet.mu.edu>
2022-03-06 10:59:49 -0600

    just to solidify it

--- xinu-hw5/include/version.h
+++ xinu-hw5/include/version.h
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-#define VERSION "(Embedded Xinu) (arm-rpi3) #39 (ekinnison@morbius) Sun Mar 6 10:54:09 CST 2022"
-=======
-<<<<<<< HEAD
-#define VERSION "(Embedded Xinu) (arm-rpi3) #10 (ecahill5@morbius) Sun Mar 6 10:32:49 CST 2022"
-=======
-#define VERSION "(Embedded Xinu) (arm-rpi3) #25 (ekinnison@morbius) Sun Mar 6 10:41:07 CST 2022"
->>>>>>> bae84beb13ad709b9378b644cb6dab75620acbea
->>>>>>> 3cd7e70bb5e7d861e5edacd31e946d87cf809b8f
+#define VERSION "(Embedded Xinu) (arm-rpi3) # (ekinnison@morbius) Sun Mar 6 10:59:15 CST 2022"
