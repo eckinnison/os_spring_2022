6ed7ff08c930a8b79d8a9e27029756904a8b44d1
Erin Cahill <ecahill5@morbius.mscsnet.mu.edu>
2022-03-20 15:25:16 -0500

    added in textbook code, modified to fit code

--- xinu-hw6/system/resched.c
+++ xinu-hw6/system/resched.c
@@ -40,6 +40,26 @@ syscall resched(void)
         }
 
     }
+
+// TEXTBOOK 9.1 CODE (editted based on our functions):
+
+    // counter: used to track if we’ve found the winner yet
+        int counter = 0;
+
+    // winner: use some call to a random number generator to
+    // get a value, between 0 and the total # of tickets
+        int winner = random(totaltickets);
+
+    // current: use this to walk through the list of jobs
+        node_t *current = head;
+        while (current) {
+            counter = counter + current->tickets;
+            if (counter > winner)
+                break; // found the winner
+            current = current->next;
+        }
+    // ’current’ is the winner: schedule it...
+
     /**
      * We recommend you use a helper function for the following:
      * TODO: Get the total number of tickets from all processes that are in current and ready states.
@@ -54,6 +74,8 @@ syscall resched(void)
     newproc=&proctab[currpid];
     newproc->state = PRCURR;    /* mark it currently running    */
     remove(currpid);
+
+    currpid = newproc; // currpid set to new process, given above, should go here or above in textbook code?
     
 
 #if PREEMPT
