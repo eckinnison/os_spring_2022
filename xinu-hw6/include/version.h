bcbf06f971c2d578ec78eb43ba79545e8674082b
Erin Cahill <ecahill5@morbius.mscsnet.mu.edu>
2022-04-01 11:23:55 -0500

    test

--- xinu-hw6/include/version.h
+++ xinu-hw6/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi3) #253 (ekinnison@morbius) Wed Mar 30 10:09:49 CDT 2022"
+#define VERSION "(Embedded Xinu) (arm-rpi3) #254 (ecahill5@morbius) Fri Apr 1 08:49:22 CDT 2022"
